c19dc80f6106143122092ab5c47f2ab83f9a127f
cherry <a1759895304@gmail.com>
2021-05-31 15:42:24 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -3,6 +3,7 @@ for ((i=1; i<=100; i++))
 do
         echo -e "\033[32m 第$i次下载 \033[0m"
         wget https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/hirsute/ubuntu-21.04-desktop-amd64.iso
+        # https://github.com/torvalds/linux/archive/refs/tags/v5.13-rc4.tar.gz
         sleep 3s
         rm ubuntu-21.04-desktop-amd64.iso
 done
