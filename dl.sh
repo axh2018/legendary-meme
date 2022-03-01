bcd5c510ae62a4cb3b600bd01708f692ecba1792
cherry <a1759895304@gmail.com>
2022-03-01 18:34:40 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo apt-get install curl wget vim git -y
+sudo apt-get install curl htop wget vim git openjdk-8-jdk maven -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install speedtest -y
 for ((i=0; i<=500; i++))
