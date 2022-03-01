c32a9796ac1051227c38d1f4e8a4aecc9c757cef
cherry <a1759895304@gmail.com>
2022-03-01 13:15:49 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-for ((i=1; i<=100; i++))
+for ((i=0; i<=500; i++))
 do
         echo -e "\033[32m 第$i次下载 \033[0m"
-        wget https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/hirsute/ubuntu-21.04-desktop-amd64.iso
+        # wget https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/hirsute/ubuntu-21.04-desktop-amd64.iso
         # https://github.com/torvalds/linux/archive/refs/tags/v5.13-rc4.tar.gz
-        sleep 3s
-        rm ubuntu-21.04-desktop-amd64.iso
+        # sleep 3s
+        # rm ubuntu-21.04-desktop-amd64.iso
+        speedtest
 done
