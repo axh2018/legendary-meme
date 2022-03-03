25d0f10a77f3570876206957ac32d2313d1c79d8
cherry <a1759895304@gmail.com>
2022-03-03 11:07:20 +0800

    Update block-netflix.sh

--- block-netflix.sh
+++ block-netflix.sh
@@ -1,5 +1,5 @@
-!#/usr/bin/bash
-# 解锁奈飞
+#!/usr/bin/bash
+# 检测流媒体解锁情况
 bash <(curl -sSL https://raw.githubusercontent.com/Netflixxp/NF/main/nf.sh)
-bash <(curl -sSL https://raw.githubusercontent.com/Netflixxp/NF/main/nf.sh)
+# 安装warp
 wget -N https://cdn.jsdelivr.net/gh/fscarmen/warp/menu.sh && bash menu.sh
